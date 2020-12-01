b1fd3e6784602e7d72b15a865ed8354904a2f533
Tommy Breslein <tommy_breslein@gmx.de>
2020-12-01 17:45:20 +0100

    finished day 1 + added interactivity

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,20 @@
 module Main where
 
-import Lib
+import Day1
+import System.Environment (getArgs)
+import System.IO (hClose, hGetContents, openFile, IOMode (ReadMode))
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    case args of
+      ["1", "1"] -> dispatch day1_1 "input/day1.txt"
+      ["1", "2"] -> dispatch day1_2 "input/day1.txt"
+      _ -> error "invalid args"
+      where
+          dispatch func fileName = do
+              handle <- openFile fileName ReadMode
+              contents <- hGetContents handle
+              putStrLn $ func contents
+              hClose handle
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,14 @@
+import Day1
+
+import Test.Hspec
+-- import Test.QuickCheck
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Day 1" $ do
+        it "given `100\\n1721\\n979\\n366\\n299\\n675\\n1456`, day1_1 should return 514579" $ do
+            day1_1 "100\n1721\n979\n366\n299\n675\n1456" `shouldBe` "514579"
+        it "given `100\\n1721\\n979\\n366\\n299\\n675\\n1456`, day1_2 should return 241861950" $ do
+            day1_2 "100\n1721\n979\n366\n299\n675\n1456" `shouldBe` "241861950"
+
+
