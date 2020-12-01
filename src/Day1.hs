4e89c8366696701c04ed1e7c443a22edfa138f9c
Tommy Breslein <tommy_breslein@gmx.de>
2020-12-01 19:16:22 +0100

    clean up

--- src/Day1.hs
+++ src/Day1.hs
@@ -8,10 +8,6 @@ import Control.Monad (replicateM)
 process :: Int -> [Int] -> Int
 process _ [] = 0
 process n xs = product . head $ [y | y <- replicateM n xs, sum y == 2020]
--- NOTE: replicateM n xs = sequence $ replicate n xs
--- thus, that call produces all n-tuples (well, technically lists, not tuples...), and filters out
--- those that match the sum x == 2020 predicate. By itself, that comprehension would give us a list
--- of all permutations of the list we are actually looking for, so head just takes the first one.
 
 day1_1 :: String -> String
 day1_1 = show . process 2 . map read . lines
