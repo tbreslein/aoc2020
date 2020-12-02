744fce3508677c7778d2130d6850306a73b4ab05
T-Doc <30618763+T-Doc@users.noreply.github.com>
2020-12-02 01:06:21 +0100

    made output a tad prettier

--- app/Main.hs
+++ app/Main.hs
@@ -8,8 +8,9 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of
-      ["1", "1"] -> dispatch day1_1 "input/day1.txt"
-      ["1", "2"] -> dispatch day1_2 "input/day1.txt"
+      ["1", "1"] -> dispatch day1_1 (getOutStr args) "input/day1.txt"
+      ["1", "2"] -> dispatch day1_2 (getOutStr args) "input/day1.txt"
       _ -> error "invalid args"
       where
-          dispatch func = putStrLn . func <=< readFile
+          dispatch func outStr = putStrLn . (++) outStr . func <=< readFile
+          getOutStr args = "Day " ++ args !! 0 ++ "; Task " ++ args !! 1 ++ "; Result = "
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,9 +6,11 @@ import Test.Hspec
 main :: IO ()
 main = hspec $ do
     describe "Day 1" $ do
-        it "given `100\\n1721\\n979\\n366\\n299\\n675\\n1456`, day1_1 should return 514579" $ do
-            day1_1 "100\n1721\n979\n366\n299\n675\n1456" `shouldBe` "514579"
-        it "given `100\\n1721\\n979\\n366\\n299\\n675\\n1456`, day1_2 should return 241861950" $ do
-            day1_2 "100\n1721\n979\n366\n299\n675\n1456" `shouldBe` "241861950"
+        it "given `1010\\n2000\\n20`, Task 1 should return 40000" $ do
+            day1_1 "1010\n2000\n20" `shouldBe` show (2000 * 20)
+        it "given `100\\n1721\\n979\\n366\\n299\\n675\\n1456`, Task 1 should return 514579" $ do
+            day1_1 "100\n1721\n979\n366\n299\n675\n1456" `shouldBe` show (1721 * 299)
+        it "given `100\\n1721\\n979\\n366\\n299\\n675\\n1456`, Task 2 should return 241861950" $ do
+            day1_2 "100\n1721\n979\n366\n299\n675\n1456" `shouldBe` show (979 * 366 * 675)
 
 
